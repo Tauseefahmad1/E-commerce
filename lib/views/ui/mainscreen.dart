fb827f8dc224963a39ab5a5e5b0628433ea9f3ea
Tauseefahmad1 <118618795+Tauseefahmad1@users.noreply.github.com>
2023-11-21 11:32:08 +0500

    Add files via upload

---
+++ lib/views/ui/mainscreen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:online_shoping/controllers/mainscreen_provider.dart';
+import 'package:online_shoping/views/shared/bottom_nav.dart';
+import 'package:online_shoping/views/shared/bottom_nav_widget.dart';
+import 'package:online_shoping/views/ui/cartpage.dart';
+import 'package:online_shoping/views/ui/homepage.dart';
+import 'package:online_shoping/views/ui/product_by_cat.dart';
+import 'package:online_shoping/views/ui/profile.dart';
+import 'package:online_shoping/views/ui/searchpage.dart';
+import 'package:provider/provider.dart';
+
+class MainScreen extends StatelessWidget {
+  MainScreen({super.key});
+
+  List<Widget> pageList = [
+    const HomePage(),
+    const SearchPage(),
+    const HomePage(),
+    CartPage(),
+    const ProfilePage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<MainScreenNotifier>(
+      builder: (context, mainScreenNotifier, child) {
+        return Scaffold(
+          backgroundColor: const Color(0xFFE2E2E2),
+          body: pageList[mainScreenNotifier.pageIndex],
+          bottomNavigationBar: const BottoNavBar(),
+        );
+      },
+    );
+  }
+}
